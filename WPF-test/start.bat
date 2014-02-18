414d27950041b781d9471d36a55cef56b565c52d
Josh Santangelo <josh@stimulant.io>
2014-02-17 18:06:00 -0800

    no cd required in start script

--- WPF-test/start.bat
+++ WPF-test/start.bat
@@ -1,5 +1,3 @@
 WHERE nodemon
 IF %ERRORLEVEL% NEQ 0 CALL npm install -g nodemon 
-
-cd ../../ampm
-nodemon -q -e js,json server.js ../ampm-test/WPF-test/config.json
+nodemon -q -e js,json ../../ampm/server.js ../ampm-test/WPF-test/config.json
