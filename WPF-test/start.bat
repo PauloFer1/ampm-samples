613f76b467ca082b43e926dae7ffcfeccea82d06
Josh Santangelo <josh@stimulant.io>
2014-03-01 14:20:36 -0800

    improved startup script

--- WPF-test/start.bat
+++ WPF-test/start.bat
@@ -1,4 +1,23 @@
-WHERE nodemon
-IF %ERRORLEVEL% NEQ 0 CALL npm install -g nodemon 
-cd ..\..\ampm
-nodemon server.js ../ampm-test/WPF-test/config.json
+:: For dev
+CD ..\..\ampm
+nodemon server.js ..\ampm-test\wpf-test\config.json
+GOTO :EOF
+
+:: Check for the node installation.
+WHERE node.exe
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO You need to install nodejs.
+	PAUSE
+	START http://nodejs.org
+	GOTO :EOF
+)
+
+:: Install nodemon.
+WHERE nodemon.cmd
+IF %ERRORLEVEL% NEQ 0 (
+	CALL npm install -g nodemon 
+)
+
+:: Launch ampm.
+CD app\ampm
+nodemon server.js ..\..\config.json
