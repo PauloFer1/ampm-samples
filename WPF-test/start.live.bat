1a950d942c69380e4164490ac44f01ad9071abc6
Josh Santangelo <josh@stimulant.io>
2014-03-03 19:23:47 -0800

    update scripts

--- WPF-test/start.live.bat
+++ WPF-test/start.live.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+
 :: Check for the node installation.
 WHERE node.exe
 IF %ERRORLEVEL% NEQ 0 (
@@ -13,10 +15,20 @@ IF %ERRORLEVEL% NEQ 0 (
 	CALL npm install -g supervisor 
 )
 
+:: Don't run if node is already running.
+TASKLIST /FI "IMAGENAME eq node.exe" 2>NUL | FIND /I /N "node.exe">NUL
+IF %ERRORLEVEL% == 0 (
+	ECHO Looks like node is already running.
+	PAUSE
+	GOTO: EOF
+)
+
 :: Run the local ampm and just watch the restart file.
 cd app\ampm
 supervisor ^
 	--watch restart.json ^
-	--ignore .git,node_modules,view,samples,logs,app,content,state.json ^
+	--ignore * ^
 	--extensions js,json ^
+	--no-restart-on error ^
+	--quiet ^
 	-- server.js ..\..\config.live.json
