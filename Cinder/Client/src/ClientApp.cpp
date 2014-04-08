dbd22c81a758c8065649b69c8a51099497e59b01
Joel Pryde <joel@stimulant.io>
2014-04-08 10:59:10 -0700

    event sending working and made all triggers be on buttons like WPF app

--- Cinder/Client/src/ClientApp.cpp
+++ Cinder/Client/src/ClientApp.cpp
@@ -9,6 +9,9 @@ using namespace std;
 
 class ClientApp : public AppNative {
 	AMPMClientRef mAMPM;
+
+	Rectf infoRect, warnRect, errorRect, eventRect, crashRect;
+
   public:
 	void setup();
 	void mouseDown( MouseEvent event );	
@@ -19,12 +22,28 @@ class ClientApp : public AppNative {
 
 void ClientApp::setup()
 {
+	float width = 100.0f;
+	float height = 40.0f;
 	mAMPM = AMPMClient::create(3002, 3003);
+	infoRect = Rectf(0, 0, width, height);
+	warnRect = Rectf(infoRect.x2, 0, infoRect.x2 + width, height);
+	errorRect = Rectf(warnRect.x2, 0, warnRect.x2 + width, height);
+	eventRect = Rectf(errorRect.x2, 0, errorRect.x2 + width, height);
+	crashRect = Rectf(eventRect.x2, 0, eventRect.x2 + width, height);
 }
 
 void ClientApp::mouseDown( MouseEvent event )
 {
-
+	if (infoRect.contains(event.getPos()))
+		LOG("testing info logging");
+	if (warnRect.contains(event.getPos()))
+		LOG_WARN("testing warn logging");
+	if (errorRect.contains(event.getPos()))
+		LOG_ERR("testing error logging");
+	if (eventRect.contains(event.getPos()))
+		AMPMClient::get()->sendEvent("category", "action", "label", 10);
+	if (crashRect.contains(event.getPos()))
+		quit();
 }
 
 void ClientApp::keyDown( KeyEvent event )
@@ -46,6 +65,23 @@ void ClientApp::draw()
 {
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) ); 
+
+	gl::color(ColorA::white());
+
+	gl::drawStrokedRoundedRect(infoRect, 8.0f, 10);
+	gl::drawStringCentered("info", infoRect.getCenter());
+
+	gl::drawStrokedRoundedRect(warnRect, 8.0f, 10);
+	gl::drawStringCentered("warn", warnRect.getCenter());
+
+	gl::drawStrokedRoundedRect(errorRect, 8.0f, 10);
+	gl::drawStringCentered("error", errorRect.getCenter());
+
+	gl::drawStrokedRoundedRect(eventRect, 8.0f, 10);
+	gl::drawStringCentered("event", eventRect.getCenter());
+
+	gl::drawStrokedRoundedRect(crashRect, 8.0f, 10);
+	gl::drawStringCentered("crash", crashRect.getCenter());
 }
 
 CINDER_APP_NATIVE( ClientApp, RendererGl )
